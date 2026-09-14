a666e32d85a583512aa55fd223349bd541c04864
Villananta <mrlucbbuck123@gmail.com>
2026-09-14 14:55:35 +0700

    feat category product

--- app/Http/Controllers/KategoriProdukController.php
+++ app/Http/Controllers/KategoriProdukController.php
@@ -2,11 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\KategoriProduk;
 use Illuminate\Http\Request;
 
 class KategoriProdukController extends Controller
 {
+    public $pageTitle = 'Kategori Produk';
     public function index(){
-        return view('kategori-produk.index');
+
+        $pageTitle = $this->pageTitle;
+        $query = KategoriProduk::query();
+        $kategori = $query->paginate(10);
+        return view('kategori-produk.index', compact('pageTitle', 'kategori'));
     }
+
 }
--- resources/views/kategori-produk/index.blade.php
+++ resources/views/kategori-produk/index.blade.php
@@ -1,6 +1,29 @@
-@extends('layouts.app')
+@extends('layouts.kai')
+@section('page_title', $pageTitle)
 @section('content')
  <div class="div card">
-    <div class="card-body py-5"></div>
+    <div class="card-body py-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th class="text-center" style="width: 15px">No</th>
+                    <th>Nama Kategori</th>
+                    <th class="text-center" style="width: 100px">Opsi</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse ($kategori as $index => $item)
+                <tr>
+                    <td>{{ $loop->iteration + $kategori->firstItem() - 1 }}</td>
+                    <td>{{ $item->nama_kategori }}</td>
+                </tr>
+                @empty
+                <tr>
+                    <td colspan="3" class="text-center">Data Kategori Kosong</td>
+                </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
  </div>
 @endsection
